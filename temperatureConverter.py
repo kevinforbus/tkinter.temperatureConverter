f03481ac479066547789d6e8e1646d40d09e440d
kevinforbus <kforbus@hotmail.com>
2026-02-27 17:02:08 -0600

    Continuing initial development

--- temperatureConverter.py
+++ temperatureConverter.py
@@ -1,9 +1,7 @@
-import sys
-sys.path.insert(0, '..')
-import converters
-# from converters import TempConverters
 import tkinter as tk
 from tkinter import ttk
+import converters
+from converters import TempConverters
 
 class App:
     def __init__(self, window):
@@ -15,19 +13,22 @@ class App:
         self.f_var = tk.DoubleVar()
         
         tk.Label(frame, text='Deg C').grid(row=0, column=0)
-        tk.Label(frame, text='Deg F').grid(row=1, column=0)
+        tk.Label(frame, text='Deg F').grid(row=1, column=0, pady=5)
         
         tk.Entry(frame, textvariable=self.c_var).grid(row=0, column=1)
         tk.Entry(frame, textvariable=self.f_var).grid(row=1, column=1)
 
-        tk.Button(frame, text='Convert', command=self.convert).grid(row=2, column=0, columnspan=2)
+        tk.Button(frame, text='Convert', command=self.convert).grid(row=2, column=0, columnspan=2, pady=10)
 
 
     def convert(self):
+        # Convert to Fahrenheit
         celsius = self.c_var.get()
         fahrenheit = self.converters.celsius_to_fahrenheit(celsius)
         self.f_var.set(fahrenheit)
+        tk.Entry(self.frame(), textvariable=self.f_var).grid(row=1, column=1)
 
+        # Convert to Celsius
         fahrenheit = self.f_var.get()
         celsius = self.converters.fahrenheit_to_celsius(fahrenheit)
         self.c_var.set(celsius)
@@ -36,7 +37,7 @@ class App:
 # Create a basic window
 window = tk.Tk()
 window.title('Temperature Converter')
-window.geometry('300x150')
+window.geometry('300x100')
 
 # Instantiate the App object
 app = App(window)
